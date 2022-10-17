5af773130e9d358545c1474896904ff12951fb38
Nicholas Goh <nicholasgbr99@gmail.com>
2022-10-17 16:55:36 +0800

    formatted the pages for netlify
    HomeView might be wrong need to compare with what daryl has actually done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="footer-basic">
-      <footer>
-          <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
-          <ul class="list-inline">
-              <li class="list-inline-item"><a href="#">Home</a></li>
-              <li class="list-inline-item"><a href="#">Services</a></li>
-              <li class="list-inline-item"><a href="#">About</a></li>
-              <li class="list-inline-item"><a href="#">Terms</a></li>
-              <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-          </ul>
-          <p class="copyright">ItsGiving © IS216</p>
-      </footer>
+    <footer>
+      <div class="social">
+        <a href="#"><i class="icon ion-social-instagram"></i></a
+        ><a href="#"><i class="icon ion-social-snapchat"></i></a
+        ><a href="#"><i class="icon ion-social-twitter"></i></a
+        ><a href="#"><i class="icon ion-social-facebook"></i></a>
+      </div>
+      <ul class="list-inline">
+        <li class="list-inline-item"><a href="#">Home</a></li>
+        <li class="list-inline-item"><a href="#">Services</a></li>
+        <li class="list-inline-item"><a href="#">About</a></li>
+        <li class="list-inline-item"><a href="#">Terms</a></li>
+        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+      </ul>
+      <p class="copyright">ItsGiving © IS216</p>
+    </footer>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="home">
-
-   <!-- navbar -->
-   <!-- end -->
-    
-
+    <!-- navbar -->
+    <!-- end -->
     <!-- header -->
     <header>
       <div class="vid">
@@ -13,30 +10,16 @@
         </video>
       </div>
       <div class="container">
-      <div class="content">
-        <h1 id="title" class="mb-0">ITS GIVING</h1>
-        <p class="mb-0">POVERTY</p>
-        <button>Learn More</button>
-      </div>
+        <div class="content">
+          <h1 id="title" class="mb-0">ITS GIVING</h1>
+          <p class="mb-0">POVERTY</p>
+          <button>Learn More</button>
+        </div>
       </div>
     </header>
     <!-- end -->
 
     <!-- 2 action tabs -->
-    <div class="three_box container">
-      <div class="row">
-         <div class="click col-6">
-            <div class="box_text">
-               <h3>Search & Apply</h3>
-               <p>ipsum dolor sit amet, consectetur adipiscing elit,<br> sed d veniam, adipiscing elit, sed d veniam</p>
-            </div>
-         </div>
-         <div class="click col-6">
-            <div class="box_text">
-               <h3>Create CSP</h3>
-               <p>ipsum dolor sit amet, consectetur adipiscing elit, <br> sed d veniam, adipiscing elit, sed d veniam</p>
-            </div>
-         </div>
     <div class="three_box">
       <div class="container">
         <div class="row">
@@ -67,7 +50,7 @@
     <div class="container" id="counters">
       <div class="row row-col-sm-1">
         <div class="counter col-6" id="csp">
-          <img src="../assets/help.png" alt="csp">
+          <img src="../assets/help.png" alt="csp" />
           <img src="../assets/teamwork.png" alt="csp" />
           <h1>10</h1>
           <p>Community Service Projects</p>
@@ -89,174 +72,106 @@
     <!-- explore icons -->
     <div class="six_box container">
       <h1>Explore</h1>
-         <div class="container">
-            <div class="row row-cols-7 justify-content-center">
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/animal.png" alt="#"/></i>
-                     <span>Animal Welfare</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx bluedark_bg">
-                     <i><img src="../assets/protect.png" alt="#"/></i>
-                     <span>Arts & Heritage</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/children.png" alt="#"/></i>
-                     <span>Children & Youth</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx bluedark_bg">
-                     <i><img src="../assets/community.png" alt="#"/></i>
-                     <span>Community</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/love.png" alt="#"/></i>
-                     <span>Disability</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/graduate-cap.png" alt="#"/></i>
-                     <span>Education</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/couple.png" alt="#"/></i>
-                     <span>Elderly</span>
-                  </div>
-               </div>
-
-      <div class="container-fluid">
-        <div class="row justify-content-center">
-          <div class="col-2 pa_left">
+      <div class="container">
+        <div class="row row-cols-7 justify-content-center">
+          <div class="col pa_left">
             <div class="six_probpx yellow_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>Shoes</span>
+              <i><img src="../assets/animal.png" alt="#" /></i>
+              <span>Animal Welfare</span>
             </div>
           </div>
-          <div class="col-2 pa_left">
+          <div class="col pa_left">
             <div class="six_probpx bluedark_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>underwear</span>
+              <i><img src="../assets/protect.png" alt="#" /></i>
+              <span>Arts & Heritage</span>
             </div>
           </div>
-          <div class="col-2 pa_left">
+          <div class="col pa_left">
             <div class="six_probpx yellow_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>Pante & socks</span>
+              <i><img src="../assets/children.png" alt="#" /></i>
+              <span>Children & Youth</span>
             </div>
           </div>
-          <div class="col-2 pa_left">
+          <div class="col pa_left">
             <div class="six_probpx bluedark_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>T-shirt & tankstop</span>
+              <i><img src="../assets/community.png" alt="#" /></i>
+              <span>Community</span>
             </div>
           </div>
-          <div class="col-2 pa_left">
+          <div class="col pa_left">
             <div class="six_probpx yellow_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>cardigans & jumpers</span>
+              <i><img src="../assets/love.png" alt="#" /></i>
+              <span>Disability</span>
+            </div>
+          </div>
+          <div class="col pa_left">
+            <div class="six_probpx yellow_bg">
+              <i><img src="../assets/graduate-cap.png" alt="#" /></i>
+              <span>Education</span>
+            </div>
+          </div>
+          <div class="col pa_left">
+            <div class="six_probpx yellow_bg">
+              <i><img src="../assets/couple.png" alt="#" /></i>
+              <span>Elderly</span>
             </div>
           </div>
         </div>
-
-            <div class="row row-cols-7 justify-content-center" id="secondrow">
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/save-the-planet.png" alt="#"/></i>
-                     <span>Envrironment</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx bluedark_bg">
-                     <i><img src="../assets/family.png" alt="#"/></i>
-                     <span>Families</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/healthcare.png" alt="#"/></i>
-                     <span>Health</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx bluedark_bg">
-                     <i><img src="../assets/aid.png" alt="#"/></i>
-                     <span>Humanitarian</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/public-service.png" alt="#"/></i>
-                     <span>Social Services</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/sports.png" alt="#"/></i>
-                     <span>Sports</span>
-                  </div>
-               </div>
-               <div class="col pa_left">
-                  <div class="six_probpx yellow_bg">
-                     <i><img src="../assets/female.png" alt="#"/></i>
-                     <span>Women & Girls</span>
-                  </div>
-               </div>
-
-        <div class="row justify-content-center" id="secondrow">
-          <div class="col-2 pa_left">
+      </div>
+      <div class="container-fluid">
+        <div class="row row-cols-7 justify-content-center" id="secondrow">
+          <div class="col pa_left">
             <div class="six_probpx yellow_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>Shoes</span>
+              <i><img src="../assets/save-the-planet.png" alt="#" /></i>
+              <span>Envrironment</span>
             </div>
           </div>
-          <div class="col-2 pa_left">
+          <div class="col pa_left">
             <div class="six_probpx bluedark_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>underwear</span>
+              <i><img src="../assets/family.png" alt="#" /></i>
+              <span>Families</span>
             </div>
           </div>
-          <div class="col-2 pa_left">
+          <div class="col pa_left">
             <div class="six_probpx yellow_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>Pante & socks</span>
+              <i><img src="../assets/healthcare.png" alt="#" /></i>
+              <span>Health</span>
             </div>
           </div>
-          <div class="col-2 pa_left">
+          <div class="col pa_left">
             <div class="six_probpx bluedark_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>T-shirt & tankstop</span>
+              <i><img src="../assets/aid.png" alt="#" /></i>
+              <span>Humanitarian</span>
             </div>
           </div>
-          <div class="col-2 pa_left">
+          <div class="col pa_left">
             <div class="six_probpx yellow_bg">
-              <i><img src="../assets/paw.png" alt="#" /></i>
-              <span>cardigans & jumpers</span>
+              <i><img src="../assets/public-service.png" alt="#" /></i>
+              <span>Social Services</span>
+            </div>
+          </div>
+          <div class="col pa_left">
+            <div class="six_probpx yellow_bg">
+              <i><img src="../assets/sports.png" alt="#" /></i>
+              <span>Sports</span>
+            </div>
+          </div>
+          <div class="col pa_left">
+            <div class="six_probpx yellow_bg">
+              <i><img src="../assets/female.png" alt="#" /></i>
+              <span>Women & Girls</span>
             </div>
           </div>
         </div>
       </div>
     </div>
     <!-- end -->
-
     <!-- footer -->
     <footer></footer>
     <!-- end -->
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-
 export default {
   name: "HomeView",
   components: {},
