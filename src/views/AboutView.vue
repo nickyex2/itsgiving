1bb8d7cb4bdd6b11c3f1fbe40261149c513c8f7f
darylyoon <84636602+darylyoon@users.noreply.github.com>
2022-10-17 16:05:00 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,121 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+
+    <!-- navbar -->
+    <!-- end -->
+    
+    <!-- about us header -->
+    <div class="section">
+        <div class="container">
+          <div class="abouttop row">
+              <div class="col-md-6">
+                <img src="../assets/image.jpeg" alt="#" />
+              </div>
+              <div class="aboutinfo col-md-6">
+                <div class="full">
+                    <h4 class="about-title">Who are we?</h4>
+                    <p class="about-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit unde placeat impedit officia, 
+                      alias commodi optio reiciendis eaque praesentium saepe libero nihil perferendis.</p>
+                    <div class="numbers row">
+                      <div class="col-md-6">
+                        <h1>100</h1>
+                        <p>CSPs listed</p>
+                      </div>
+                      <div class="col-md-6">
+                        <h1>200</h1>
+                        <p>Sign Ups</p>
+                      </div>
+                    </div>
+                </div>
+              </div>
+          </div>
+        </div>
+    </div>
+    <!-- end -->
+
+    <!-- what we do/how it works -->
+    <div class="hiw container">
+      <h1>How it works</h1>
+        <div class="how-it-works row row-cols-2">
+          <div class="col">
+            <img src="../assets/hand-up.png" alt="search">
+            <h4>Volunteer</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque omnis quasi saepe repudiandae ad, 
+              deserunt ipsum ipsa totam esse nihil explicabo est quia labore accusantium amet ducimus quidem laborum laudantium!</p>
+          </div>
+          <div class="col">
+            <img src="../assets/home.png" alt="search">
+            <h4>Beneficiaries</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque omnis quasi saepe repudiandae ad, 
+              deserunt ipsum ipsa totam esse nihil explicabo est quia labore accusantium amet ducimus quidem laborum laudantium!</p>
+          </div>
+          <!-- <div class="col">
+            <img src="../assets/paw.png" alt="search">
+            <h4>Volunteer</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque omnis quasi saepe repudiandae ad, 
+              deserunt ipsum ipsa totam esse nihil explicabo est quia labore accusantium amet ducimus quidem laborum laudantium!</p>
+          </div> -->
+        </div>
+    </div>
+    <!-- end -->
+
+    <!-- sign up now -->
+    <div class="signupbanner container">
+      <div class="aboutvid vid">
+        <video autoplay muted loop id="myVideo">
+          <source src="../assets/vid2.mp4" type="video/mp4">
+        </video>
+        <p>Remember you choose <br>who to support, <br>how to support them, <br>and when inspiration strikes, <br>we’re the platform to kick off your giving dreams and projects!</p>
+        <button>Sign Up Now</button>
+      </div>
+    </div>
+
+    <!-- meet our team -->
+    <div class="container">
+      <h1 class="mtthead">Meet The Team</h1>
+      <div class="mtt row row-cols-lg-4 row-cols-2 g-4">
+        <div class="col">
+          <div class="card h-100">
+            <img src="../assets/mok.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Colin Mok</h5>
+              <p class="card-text">Web Developer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src="../assets/yoon.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Daryl Yoon</h5>
+              <p class="card-text">Web Developer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src="../assets/lai.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Daniel Lai</h5>
+              <p class="card-text">Web Developer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+            <img src="../assets/nicky.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Nicholas Goh</h5>
+              <p class="card-text">Web Developer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end -->
+
+    <!-- footer -->
+    <!-- end -->
+
   </div>
 </template>
-
-<script>
-export default {
-  name: "AboutView",
-  setup() {},
-};
-</script>
