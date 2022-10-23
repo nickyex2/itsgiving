46d3a5ab10deb3d9a1f4f7c2e66b4bb4c7073f99
molincok <colinmok1000@gmail.com>
2022-10-24 01:02:56 +0800

    added located me

--- src/views/SearchResults.vue
+++ src/views/SearchResults.vue
@@ -24,24 +24,27 @@
               v-model="autocompleteaddress"
               placeholder="Want to Search By Address?"
               ref="autocompleteaddress"
+              style="width: 85%"
             />
             <button
               type="button"
-              class="btn btn-danger"
+              class="btn btn-danger py-0"
               @click="locatorButtonPressed"
+              style="width: 15%"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
+                width="25"
+                height="25"
                 fill="currentColor"
-                class="bi bi-cursor"
+                class="bi bi-cursor pt-2"
                 viewBox="0 0 16 16"
               >
                 <path
                   d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103zM2.25 8.184l3.897 1.67a.5.5 0 0 1 .262.263l1.67 3.897L12.743 3.52 2.25 8.184z"
                 />
               </svg>
+              <p style="font-size: 0.6rem; margin: 0" class="pb-2">Locate Me</p>
             </button>
           </div>
           <div class="col-1 form-check form-switch">
