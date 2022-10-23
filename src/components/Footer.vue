bc84c563d1c6704ccff66e0e88c18c133f995bd0
darylyoon <84636602+darylyoon@users.noreply.github.com>
2022-10-23 16:44:18 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,17 +3,9 @@
     <footer>
       <div class="social">
         <a href="#"><i class="icon ion-social-instagram"></i></a
-        ><a href="#"><i class="icon ion-social-snapchat"></i></a
         ><a href="#"><i class="icon ion-social-twitter"></i></a
         ><a href="#"><i class="icon ion-social-facebook"></i></a>
       </div>
-      <ul class="list-inline">
-        <li class="list-inline-item"><a href="#">Home</a></li>
-        <li class="list-inline-item"><a href="#">Services</a></li>
-        <li class="list-inline-item"><a href="#">About</a></li>
-        <li class="list-inline-item"><a href="#">Terms</a></li>
-        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-      </ul>
       <p class="copyright">ItsGiving © IS216</p>
     </footer>
   </div>
