9984920f3ef15a6446ceba80e5dc137bc81bd2f4
Nicholas Goh <nicholasgbr99@gmail.com>
2022-11-07 03:37:53 +0800

    styled approval
    approval csp logic half done, left storing into db for users and availability

--- src/components/ApprovalCsp.vue
+++ src/components/ApprovalCsp.vue
@@ -1,7 +1,6 @@
 <template>
-  <h1>this is the approval table for owner</h1>
-  <div class="d-flex justify-content-center">
-    <table border="1" class="table table-dark w-75" v-if="applicants != null">
+  <div class="d-flex justify-content-center approvalcsp container col-8 mb-5">
+    <table class="table table-responsive-md" v-if="applicants != null">
       <thead>
         <tr>
           <th>Name</th>
@@ -13,10 +12,10 @@
       </thead>
       <tbody>
         <tr v-for="(applicant, id) in applicants" :key="id">
-          <td>{{ applicant.full_name }}</td>
-          <td>{{ applicant.email }}</td>
-          <td>{{ applicant.datetime }}</td>
-          <td>{{ applicant.status }}</td>
+          <td class="tabled">{{ applicant.full_name }}</td>
+          <td class="tabled">{{ applicant.email }}</td>
+          <td class="tabled">{{ applicant.datetime }}</td>
+          <td class="tabled">{{ applicant.status }}</td>
           <td>
             <button
               class="btn btn-success me-2"
@@ -45,15 +44,81 @@ import { getDatabase, ref as dbRefe, onValue } from "firebase/database";
 import { onBeforeMount, ref } from "vue";
 export default {
   name: "ApprovalCsp-component",
-  props: ["cspID"],
+  props: ["cspID", "cspHours"],
   setup(props) {
     const db = getDatabase();
     const applicants = ref({}); //need to merge the nested objects into one big object
-    const approve = (id, status) => {
+    const approve = async (id, status) => {
       console.log("approve", id, status);
+      const userRef = dbRefe(db, `users/${id}`);
+      const data = ref({});
+      const pending_csp = ref([]);
+      const approved_csp = ref([]);
+      onValue(userRef, (snapshot) => {
+        if (snapshot.val()) {
+          data.value = snapshot.val();
+          pending_csp.value = data.value.pending_csp;
+          if (data.value.approved_csp) {
+            approved_csp.value = data.value.approved_csp;
+          }
+        }
+      });
+      // remove cspid from pending_csp
+      pending_csp.value = pending_csp.value.filter((csp) => csp != props.cspID);
+      if (pending_csp.value.length == 0) {
+        pending_csp.value = false;
+      }
+      // add cspid to approved_csp
+      approved_csp.value.push(props.cspID);
+      console.log("pending_csp", pending_csp.value);
+      console.log("approved_csp", approved_csp.value);
+      // update the user's data
+      // await update(userRef, {
+      //   pending_csp: pending_csp.value,
+      //   approved_csp: approved_csp.value,
+      //   hours: data.value.hours + props.cspHours,
+      // });
+      // update the availability data for the csp
+      const availRef = dbRefe(db, `availability/${props.cspID}`);
+      const availData = ref({});
+      onValue(availRef, (snapshot) => {
+        if (snapshot.val()) {
+          availData.value = snapshot.val();
+        }
+      });
+      // change the applicant status to approved from the availability data
     };
     const reject = (id, status) => {
       console.log("reject", id, status);
+      const userRef = dbRefe(db, `users/${id}`);
+      const data = ref({});
+      const pending_csp = ref([]);
+      const rejected_csp = ref([]);
+      onValue(userRef, (snapshot) => {
+        if (snapshot.val()) {
+          data.value = snapshot.val();
+          pending_csp.value = data.value.pending_csp;
+          if (data.value.approved_csp) {
+            rejected_csp.value = data.value.rejected_csp;
+          }
+        }
+      });
+      // remove cspid from pending_csp
+      pending_csp.value = pending_csp.value.filter((csp) => csp != props.cspID);
+      if (pending_csp.value.length == 0) {
+        pending_csp.value = false;
+      }
+      // add cspid to approved_csp
+      rejected_csp.value.push(props.cspID);
+      console.log("pending_csp", pending_csp.value);
+      console.log("rejected_csp", rejected_csp.value);
+      // update the user's data
+      // await update(userRef, {
+      //   pending_csp: pending_csp.value,
+      //   approved_csp: approved_csp.value,
+      //   hours: data.value.hours + props.cspHours,
+      // });
+      // update the availability data for the csp
     };
     onBeforeMount(async () => {
       const applicantsRef = dbRefe(
--- src/views/CSPView.vue
+++ src/views/CSPView.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="container-fluid my-5 justify-content-center">
-    <div v-if="editAccess" class="row w-25 ms-auto me-2">
-      <button class="btn btn-warning mt-3" @click="handleEditCSP">edit</button>
-    </div>
+  <div class="container-fluid mt-5 justify-content-center">
     <div class="cspContent container py-3">
       <div class="row justify-content-center my-5">
         <div class="col-md-7 col-12">
@@ -52,7 +49,7 @@
           </div>
           <!-- end of dummy carousel -->
 
-          <div class="apply-csp my-4">
+          <div class="apply-csp my-4" v-if="!editAccess">
             <div class="mt-2 text-center">
               <h5>Interview Timeslots available:</h5>
             </div>
@@ -85,7 +82,23 @@
 
         <div class="col-md-5 col-12">
           <div class="csp-deets mb-3">
-            <h2>{{ csp.name }}</h2>
+            <h2 class="d-inline">{{ csp.name }}</h2>
+            <span class="float-end">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-pencil-fill pencil-edit"
+                v-if="editAccess"
+                viewBox="0 0 16 16"
+                @click="handleEditCSP"
+              >
+                <path
+                  d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"
+                />
+              </svg>
+            </span>
             <h5>by {{ csp.owner }}</h5>
             <p>{{ csp.description }}</p>
           </div>
@@ -283,7 +296,7 @@
       </div>
     </div>
   </div>
-  <ApprovalCsp v-else :cspID="cID"></ApprovalCsp>
+  <ApprovalCsp v-else :cspID="cID" :cspHours="csp.csp_hours"></ApprovalCsp>
   <!-- end -->
 </template>
 
